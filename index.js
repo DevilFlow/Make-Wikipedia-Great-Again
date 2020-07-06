bf76de6c8c7a83e7ada854216bb358d7cde98403
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2020-07-06 12:22:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fetch = require('node-fetch');
-
+response.writeHead(302 , {
+           'Location' : 'https://make-wikipedia-great-again.vercel.app/wiki' // This is your url which you want
+        });
+response.end();
 module.exports = async (req, res) => {
   res.setHeader('Cache-Control', 's-maxage=3, stale-while-revalidate');
   const html = (await (await fetch(`https://wikipedia.org${req.url}`)).text())
