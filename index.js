f0fe6186c6284670db9abac92aabf26266178d13
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2020-07-06 12:13:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ let headers = new Headers({
 });
 module.exports = async (req, res) => {
   res.setHeader('Cache-Control', 's-maxage=3, stale-while-revalidate');
-  const html = (await (await fetch(`https://wikipedia.org${req.url}`, {
-    method  : 'GET', 
-    headers : headers 
-}).text())
+  const html = (await (await fetch(`https://wikipedia.org${req.url}`)).text())
     .replace(/(href=.)https?:\/\/wikipedia.org/g, `$1//${req.headers.host}`)
     .replace(
       '</head>',
