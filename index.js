9e154a0f580602106d2bf5c9447fa1c3e80c1405
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2020-07-06 12:01:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
   res.setHeader('Cache-Control', 's-maxage=3, stale-while-revalidate');
-  const html = (await (await fetch(`https://wikipedia.org${req.url}`)).text())
+  const html = (await (await   fetch(url, {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.100 Safari/534.30' // <---
+}).text())
     .replace(/(href=.)https?:\/\/wikipedia.org/g, `$1//${req.headers.host}`)
     .replace(
       '</head>',
@@ -11,3 +13,9 @@ module.exports = async (req, res) => {
 
   res.end(html);
 };
+/*  const html = (await (await fetch(`https://wikipedia.org${req.url}`)).text())
+    .replace(/(href=.)https?:\/\/wikipedia.org/g, `$1//${req.headers.host}`)
+    .replace(
+      '</head>',
+      '<link media="all" href="/wiki.css" rel="stylesheet" /></head>'
+    );*/
