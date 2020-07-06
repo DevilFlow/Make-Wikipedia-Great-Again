ce19afd95bad363718a04ccfcf8151551192096d
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2020-07-06 12:15:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const fetch = require('node-fetch');
-let headers = new Headers({
-    "User-Agent"   : "MY-UA-STRING"
-});
+
 module.exports = async (req, res) => {
   res.setHeader('Cache-Control', 's-maxage=3, stale-while-revalidate');
   const html = (await (await fetch(`https://wikipedia.org${req.url}`)).text())
@@ -13,9 +11,3 @@ module.exports = async (req, res) => {
 
   res.end(html);
 };
-/*  const html = (await (await fetch(`https://wikipedia.org${req.url}`)).text())
-    .replace(/(href=.)https?:\/\/wikipedia.org/g, `$1//${req.headers.host}`)
-    .replace(
-      '</head>',
-      '<link media="all" href="/wiki.css" rel="stylesheet" /></head>'
-    );*/
